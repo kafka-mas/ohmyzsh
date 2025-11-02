6e45a76f592d94f2729d2b79dc7b0fcb3432ab0c
Kafka <kafka_mas@proton.me>
2025-11-02 17:40:16 +0300

    Edit installation script

--- tools/install.sh
+++ tools/install.sh
@@ -70,7 +70,7 @@ fi
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-kafka-mas/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
